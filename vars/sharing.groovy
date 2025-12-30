121610e9284ee9e6eeb5bd6f7096e0fa42f5b3eb
ram-2699 <akularamkiran99@gmail.com>
2025-12-30 15:12:16 -0500

    a

---
+++ vars/sharing.groovy
@@ -0,0 +1,18 @@
+def down(repo)
+{
+  git 'https://github.com/IntelliqDevops/${repo}.git'
+}
+
+def build()
+{
+  sh 'mvn package'
+}
+
+def deploy(name, ip, aname)
+{
+  sh 'scp /var/lib/jenkins/workspace/${name}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/${aname}.war'
+}
+def selinium(name)
+{
+  sh 'java -jar /var/lib/jenkins/workspace/${name}/testing.jar'
+}
